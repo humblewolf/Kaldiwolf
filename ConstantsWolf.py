4a4319f1a20b1fd55141b9a166d03237db14f23e
humblewolf <amarjeet23malik@gmail.com>
2018-09-18 14:35:45 +0530

    config file made more easy to understand

--- ConstantsWolf.py
+++ ConstantsWolf.py
@@ -7,41 +7,42 @@ import pyaudio
 
 class ConstantsWolf:
 
+    ########################## master and worker node configs #########################
+
     # pyro
 
     pyro_node_name_prefix = "kaldiwolf_pyro_node"
     max_pyro_nodes = 100
-    pyro_ns_address = "10.131.10.64"  # this must be on an all-visible node
+    pyro_ns_address = "10.131.10.64"  # this must be on an all-visible node <<<<<<<<<<<<<<<<-------------------
 
-    #  read from wav file settings
-
-    sampling_rate = 8000
-    packet_length_ms = 30
-    bytes_per_sample = 2
-    channels = 1
-    test_file_wav = "D:\\anagram\\asterisk-intg\\ivrag\\31512.wav"
-    loop_sleep_secs = 0.001 # 0.001 gives best for 2 good nodes
 
     # server configs
 
-    ws_server_host = "127.0.0.1"
-    ws_server_port = 9000
     redis_server_host = "10.131.10.64"  # not used , if you want to change it change redispy initalization in Redisqueuewolf
     redis_server_port = 6379  # not used , if you want to change it change redispy initalization in Redisqueuewolf
-    client_log_loc = "logs/client.log"
-    srv_log_loc = "logs/server.log"
     srv_tcpt_queue_block_timeout = 3
 
-    #  vad settings
+    ########################## only master node configs #########################
 
+    #  vad settings
     vad_agressiveness = 2  # 2 for file decoding, 3 for mic decoding
     """set its aggressiveness mode, which is an integer between 0 and 3.
      0 is the least aggressive about filtering out non-speech, 3 is the most aggressive"""
     padding_duration_ms = 300
 
+    srv_log_loc = "logs/server.log"
+
+    ########################## only worker node configs #########################
+
     # decoding settings
     kaldi_home = "/home/humblewolf/kaldi-asr/"  # this path should have trailing slashes
-    decode_script_name = "decode_kw.sh"
+    decode_script_name = "kw_decode_sno.sh"
+
+    ######################## client only settings ###############################
+
+    ws_server_host = "127.0.0.1"
+    ws_server_port = 9000
+    client_log_loc = "logs/client.log"
 
     #  read from microphone, configs
 
@@ -53,3 +54,12 @@ class ConstantsWolf:
     channels_mic = 1
     test_record_seconds = 10
     loop_sleep_secs_mic = 0.002
+
+    #  read from wav file settings
+
+    sampling_rate = 8000
+    packet_length_ms = 30
+    bytes_per_sample = 2
+    channels = 1
+    test_file_wav = "D:\\anagram\\asterisk-intg\\ivrag\\31512.wav"
+    loop_sleep_secs = 0.001  # 0.001 gives best for 2 good nodes
